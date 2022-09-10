5eb9fe7dbb930aa98cb17f842e938e72ff1848eb
yiyanglin0102 <yiyanglin0102@gmail.com>
2022-09-10 12:49:37 -0400

    Create getter setter in Chatacter File

--- HW1/GorillaGame/src/Chatacter.java
+++ HW1/GorillaGame/src/Chatacter.java
@@ -1,66 +1,87 @@
 public class Chatacter {
-    
-    public static int TOTAL_OF_GAME_THROW = 0;
+    public static int DEFAULT_ANGLE = 45;
+    public static int DEFAULT_POWER = 30;
+    public static String DEFAULT_NAME = "Player";
+    public static int NUM_OF_PLAYERS;
+
     private String name = "";
-    private int angle = 0;
-    private int power = 0;
-    private int threwTimes = 0;
     private int position_x = 0;
     private int position_y = 0;
     private int score = 0;
+    private int power = 0;
+    private int playerThrewTimes = 0;
+    private int angle = 0;
 
-    public Chatacter()
-    {
-        System.out.println(name);
+    public Chatacter() {
+        this.name = DEFAULT_NAME;
+        this.position_x = 0;
+        this.position_y = 0;
+        this.score = 0;
+        this.power = DEFAULT_POWER;
+        NUM_OF_PLAYERS += 1;
+        this.playerThrewTimes = 0;
     }
 
-    public Chatacter(String name, int position_x, int position_y)
-    {
+    public Chatacter(String name, int position_x, int position_y) {
         this.name = name;
         this.position_x = position_x;
         this.position_y = position_y;
         this.score = 0;
-        this.threwTimes = 0;
-        this.power = 0;
+        this.power = DEFAULT_POWER;
+        NUM_OF_PLAYERS += 1;
+        this.playerThrewTimes = 0;
     }
 
-    public void print()
-    {
+    public void printINFO() {
         System.out.println("Player name: " + this.name);
         System.out.println("Player position_x: " + this.position_x);
         System.out.println("Player position_y: " + this.position_y);
         System.out.println("Player score: " + this.score);
-        System.out.println("Player threw times: " + this.threwTimes);
+        System.out.println("Player threw times: " + this.playerThrewTimes);
         System.out.println("Player power: " + this.power);
     }
 
-    public int getPosition_x()
-    {
+    public int getPosition_x() {
         return this.position_x;
     }
-    
-    public int getPosition_y()
-    {
+
+    public int setPosition_x(int input_position_x) {
+        this.position_x = input_position_x;
+        return this.position_x;
+    }
+
+    public int getPosition_y() {
         return this.position_y;
     }
 
-    public int getScore()
-    {
+    public int setPosition_y(int input_position_y) {
+        this.position_y = input_position_y;
+        return this.position_y;
+    }
+
+    public int getScore() {
         return this.score;
     }
 
-    public int getPower()
-    {
+    public int getPower() {
         return this.power;
     }
 
-    public int getThrewTimes()
-    {
-        return this.threwTimes;
+    public int setPower(int input_power) {
+        this.power = input_power;
+        return this.power;
     }
 
-    public int angle()
-    {
+    public int getThrewTimes() {
+        return this.playerThrewTimes;
+    }
+
+    public int getAngle() {
+        return this.angle;
+    }
+
+    public int setAngle(int input_angle) {
+        this.angle = input_angle;
         return this.angle;
     }
 
--- HW1/GorillaGame/src/GameStart.java
+++ HW1/GorillaGame/src/GameStart.java
@@ -1,7 +1,6 @@
 public class GameStart {
-    
     public static int TOTAL_OF_GAME_THROW = 0;
-    public final int NUM_OF_PLAYERS = 2;
+    public final int NUM_OF_PLAYERS = 0;
     
     public GameStart()
     {
