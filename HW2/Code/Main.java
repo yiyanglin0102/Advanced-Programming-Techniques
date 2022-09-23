68c78ca7a1856dd4fa11678f3b6a905216ebab5f
yiyanglin0102 <yiyanglin0102@gmail.com>
2022-09-23 10:00:46 -0400

    Created HW2 and added index to merged file

---
+++ HW2/Code/Main.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.ArrayList;
+
+// import java.util.*;
+
+public class Main {
+
+  public static Character role;
+
+  public static void main(String[] args) throws Exception {
+    // int argc = args.length;
+    ArrayList<String> list = new ArrayList<>();
+    for (String s : args) {
+      list.add(s);
+    }
+
+    ArrayList<FileReader> listOfFiles = new ArrayList<>();
+    for (String s : list) {
+      FileReader inputFile = new FileReader(s);
+      listOfFiles.add(inputFile);
+    }
+    FileWriter writer = new FileWriter("mergedFile.csv");
+    BufferedWriter buffer = new BufferedWriter(writer);
+
+    int index = 0;
+
+    for (FileReader f : listOfFiles) {
+      BufferedReader br = new BufferedReader(f);
+      int i;
+      buffer.write(String.valueOf(index) + " ");
+      
+
+      while ((i = br.read()) != -1) {
+        buffer.write((char) i);
+        if ((char) i == '\n') {
+          index += 1;
+          buffer.write(String.valueOf(index) + " ");
+        }
+      }
+      index += 1;
+      buffer.newLine();
+
+      // br.close();
+      // f.close();
+      System.out.println();
+    }
+
+    // writer.close();
+    buffer.close();
+    System.out.println("Success");
+  }
+}
