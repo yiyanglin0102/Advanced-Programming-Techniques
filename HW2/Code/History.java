49b912af896483a62c09639a463948a6a4152272
yiyanglin0102 <yiyanglin0102@gmail.com>
2022-09-23 23:15:32 -0400

    Added timestamp and added checking history to makefile

--- HW2/Code/History.java
+++ HW2/Code/History.java
@@ -1,12 +1,31 @@
 import java.io.*;
-import java.util.*;
+// import java.util.*;
+import java.util.regex.Pattern;
 
 public class History {
 
   public static void main(String[] args) throws Exception {
-    
-    
+    checkHistory(args[0]);
+  }
 
+  public static void checkHistory(String input) throws IOException {
+    FileReader inputFile = new FileReader(".history");
+    BufferedReader br = new BufferedReader(inputFile);
+    int count = 0;
+    String line = br.readLine();
+    while (line != null) {
+      if (
+        Pattern
+          .compile(Pattern.quote(input), Pattern.CASE_INSENSITIVE)
+          .matcher(line)
+          .find()
+      ) {
+        count += 1;
+      }
 
+      line = br.readLine();
+    }
+    System.out.println(input + ", " + count + " times in searching history...");
+    br.close();
   }
 }
--- HW2/Code/Search.java
+++ HW2/Code/Search.java
@@ -1,5 +1,6 @@
 import java.io.*;
-// import java.util.*;
+import java.sql.*;
+import java.util.Date;
 import java.util.regex.Pattern;
 
 public class Search {
@@ -7,7 +8,7 @@ public class Search {
   public static void main(String[] args) throws Exception {
     String file = "mergedFile.csv";
     addHistory(args[1]);
-    checkHistory(args[1]);
+
     search(args[1], file);
   }
 
@@ -33,34 +34,15 @@ public class Search {
 
   public static void addHistory(String input) throws IOException {
     FileWriter writerHistory = new FileWriter(".history", true);
+    Date date = new Date();
+    Timestamp timestamp = new Timestamp(date.getTime());
 
     try (
       BufferedWriter historyBufferWriter = new BufferedWriter(writerHistory)
     ) {
-      historyBufferWriter.write("search " + input);
+      historyBufferWriter.write("search " + input + " " + timestamp);
       historyBufferWriter.newLine();
       historyBufferWriter.close();
     }
-  }
-
-  public static void checkHistory(String input) throws IOException {
-    FileReader inputFile = new FileReader(".history");
-    BufferedReader br = new BufferedReader(inputFile);
-    int count = 0;
-    String line = br.readLine();
-    while (line != null) {
-      if (
-        Pattern
-          .compile(Pattern.quote(input), Pattern.CASE_INSENSITIVE)
-          .matcher(line)
-          .find()
-      ) {
-        count += 1;
-      }
-
-      line = br.readLine();
-    }
-    System.out.println("Searched " + input + " " + count + " times...");
-    br.close();
   }
 }
