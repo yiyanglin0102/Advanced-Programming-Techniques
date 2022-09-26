85ec1baf91f71b8bc9d1be7ded0859e5f3d37a75
yiyanglin0102 <yiyanglin0102@gmail.com>
2022-09-26 10:28:57 -0400

    Added no .history exception and change search history result

--- HW2/Code/History.java
+++ HW2/Code/History.java
@@ -1,5 +1,4 @@
 import java.io.*;
-// import java.util.*;
 import java.util.regex.Pattern;
 
 public class History {
@@ -9,29 +8,32 @@ public class History {
   }
 
   public static void checkHistory(String input) throws IOException {
-    FileReader inputFile = new FileReader(".history");
-    BufferedReader br = new BufferedReader(inputFile);
-    int count = 0;
-    String line = br.readLine();
-    while (line != null) {
-      if (
-        Pattern
-          .compile(Pattern.quote(input), Pattern.CASE_INSENSITIVE)
-          .matcher(line)
-          .find()
-      ) {
-        count += 1;
+    try {
+      FileReader inputFile = new FileReader(".history");
+      BufferedReader br = new BufferedReader(inputFile);
+      int count = 0;
+      String line = br.readLine();
+      while (line != null) {
+        if (
+          input.toUpperCase().equals(line.split(" ")[1].toUpperCase())
+        ) {
+          count += 1;
+        }
+
+        line = br.readLine();
+      }
+      System.out.println(
+        input + ", " + count + " times in searching history..."
+      );
+
+      if (count > 0) {
+        printTimestamp(input);
       }
 
-      line = br.readLine();
+      br.close();
+    } catch (IOException ioe) {
+      System.out.println("Exception caught: No searching history found...");
     }
-    System.out.println(input + ", " + count + " times in searching history...");
-
-    if (count > 0) {
-      printTimestamp(input);
-    }
-
-    br.close();
   }
 
   public static void printTimestamp(String input) throws IOException {
