add9e6afc5d5625e11d6815ea8a4393ff6ec5282
yiyanglin0102 <yiyanglin0102@gmail.com>
2022-10-30 11:24:31 -0400

    Added comments

--- HW4/Code/Main.java
+++ HW4/Code/Main.java
@@ -3,7 +3,10 @@ import java.io.*;
 public class Main {
 
   public static void main(String[] args) throws IOException {
+
+    // Used to create operating system processes.
     ProcessBuilder pb = new ProcessBuilder();
+    // Get the file
     pb.command(new File(".").getCanonicalPath() + "/" + args[0]);
     Process process = pb.start();
     String result = read(process);
