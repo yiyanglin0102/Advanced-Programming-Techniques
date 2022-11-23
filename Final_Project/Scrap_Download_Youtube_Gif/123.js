ba77e8ea84e63e93c0296eb8d5485f567c35099f
GxhCHULLL <yiyanglin0102@gmail.com>
2022-11-23 02:36:30 -0500

    Created HTML UI for YT with script.js

---
+++ Final_Project/Scrap_Download_Youtube_Gif/123.js
@@ -0,0 +1,34 @@
+let object = {link0:"https://www.youtube.com/watch?v=UKHd4V664ts", link1:"https://www.youtube.com/watch?v=9PBZy9j3H3I", link2:"https://www.youtube.com/watch?v=6nPYNMwWOnk", link3:"https://www.youtube.com/watch?v=_lrpCJOV4Fg", link4:"https://www.youtube.com/watch?v=omEuqt19Uhs", link5:"https://www.youtube.com/watch?v=0pObLdN9ICg", link6:"https://www.youtube.com/watch?v=UZWmtxLiiFE", link7:"https://www.youtube.com/watch?v=FdfipkGBLd0", link8:"https://www.youtube.com/watch?v=PxeQfYWikvI", link9:"https://www.youtube.com/watch?v=fyrlyMsBrgg"}
+
+function function0() {
+  window.location.assign(object.goo);
+}
+function function1() {
+  window.location.assign(object.goo);
+}
+function function2() {
+  window.location.assign(object.goo);
+}
+function function3() {
+  window.location.assign(object.goo);
+}
+function function4() {
+  window.location.assign(object.goo);
+}
+function function5() {
+  window.location.assign(object.goo);
+}
+function function6() {
+  window.location.assign(object.goo);
+}
+
+function function7() {
+  window.location.assign(object.goo);
+}
+
+function function8() {
+  window.location.assign(object.goo);
+}
+function function9() {
+  window.location.assign(object.goo);
+}
---
+++ Final_Project/htmlUI_YT/script.js
@@ -0,0 +1,35 @@
+let object = {link0:"https://www.youtube.com/watch?v=LgY3c5tI36s", link1:"https://www.youtube.com/watch?v=hreDzgEpvKA", link2:"https://www.youtube.com/watch?v=yj2oWUUpzaU", link3:"https://www.youtube.com/watch?v=9jLN8MQRQh0", link4:"https://www.youtube.com/watch?v=AF_d3rVDS7I", link5:"https://www.youtube.com/watch?v=lPZQLQavG4A", link6:"https://www.youtube.com/watch?v=2V93R2qRwt8", link7:"https://www.youtube.com/watch?v=veex4hF7wj4", link8:"https://www.youtube.com/watch?v=xcxqSf2H_yI", link9:"https://www.youtube.com/watch?v=nQg0cuzN0mQ"}
+
+
+function function0() {
+  window.location.assign(object.link0);
+}
+function function1() {
+  window.location.assign(object.link1);
+}
+function function2() {
+  window.location.assign(object.link2);
+}
+function function3() {
+  window.location.assign(object.link3);
+}
+function function4() {
+  window.location.assign(object.link4);
+}
+function function5() {
+  window.location.assign(object.link5);
+}
+function function6() {
+  window.location.assign(object.link6);
+}
+
+function function7() {
+  window.location.assign(object.link7);
+}
+
+function function8() {
+  window.location.assign(object.link8);
+}
+function function9() {
+  window.location.assign(object.link9);
+}
