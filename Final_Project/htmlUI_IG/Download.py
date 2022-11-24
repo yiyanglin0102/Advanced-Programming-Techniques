dcef49bc2e7932af0d6fa3821ae98efa2e5b8309
GxhCHULLL <yiyanglin0102@gmail.com>
2022-11-24 10:57:30 -0500

    Created IG HTML UI

---
+++ Final_Project/htmlUI_IG/Download.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+import os, sys, re, subprocess, requests, numpy as np
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+
+subprocess.run(('rm -rf 0 1 2 3 4 5 6 7 8 9'), shell=True)
+
+# keyWord = 'love'
+keyWord = sys.argv[1]
+
+
+def saveFullHtmlPage(url, pagepath='page', session=requests.Session(), html=None):
+    """Save web page html and supported contents        
+        * pagepath : path-to-page   
+        It will create a file  `'path-to-page'.html` and a folder `'path-to-page'_files`
+    """
+    def savenRename(soup, pagefolder, session, url, tag, inner):
+        if not os.path.exists(pagefolder): # create only once
+            os.mkdir(pagefolder)
+        for res in soup.findAll(tag):   # images, css, etc..
+            if res.has_attr(inner): # check inner tag (file object) MUST exists  
+                try:
+                    filename, ext = os.path.splitext(os.path.basename(res[inner])) # get name and extension
+                    filename = re.sub('\W+', '', filename) + ext # clean special chars from name
+                    fileurl = urljoin(url, res.get(inner))
+                    filepath = os.path.join(pagefolder, filename)
+                    # rename html ref so can move html and folder of files anywhere
+                    res[inner] = os.path.join(os.path.basename(pagefolder), filename)
+                    if not os.path.isfile(filepath): # was not downloaded
+                        with open(filepath, 'wb') as file:
+                            filebin = session.get(fileurl)
+                            file.write(filebin.content)
+                except Exception as exc:
+                    print(exc, file=sys.stderr)
+    if not html:
+        html = session.get(url).text
+    soup = BeautifulSoup(html, "html.parser")
+    path, _ = os.path.splitext(pagepath)
+    pagefolder = path+'_files' # page contents folder
+    tags_inner = {'img': 'src', 'link': 'href', 'script': 'src'} # tag&inner tags to grab
+    for tag, inner in tags_inner.items(): # saves resource files and rename refs
+        savenRename(soup, pagefolder, session, url, tag, inner)
+    with open(path+'.html', 'wb') as file: # saves modified html doc
+        file.write(soup.prettify('utf-8'))
+
+saveFullHtmlPage('https://www.youtube.com/results?search_query=' + keyWord, keyWord)
+
+with open(keyWord + ".html") as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+
+# "videoIds": ["
+
+a = soup.select("body")[0]
+
+paragraphs = []
+for x in a:
+    paragraphs.append(str(x))
+
+for i, a in enumerate(paragraphs):
+    if "\"videoIds\":[\"" in a:
+        b = paragraphs[31]
+
+
+match=(re.search("videoIds", b))
+
+result = [_.start() for _ in re.finditer("\"videoIds\":", b)]
+
+result = np.add(result, 13).tolist()
+videos = set()
+for n in result:
+    videos.add(b[n:n+11])
+
+videos = list(videos)
+for i in range(len(videos)):
+    videos[i] = "https://www.youtube.com/watch?v=" + videos[i]
+#     videos[i] = "youtube.com/watch?v=" + videos[i]
+videos
+
+subprocess.run(('rm '+ keyWord + '.html'), shell=True)
+subprocess.run(('rm -rf '+ keyWord + '_files'), shell=True)
+
+
+# In[10]:
+
+
+videos = videos[0:10]
+videos
+
+
+# In[ ]:
+
+
+
+
+
+# In[20]:
+
+
+def delete_first_line(filename):
+    with open(filename, 'r') as fin:
+        data = fin.read().splitlines(True)
+    with open(filename, 'w') as fout:
+        fout.writelines(data[1:])
+        
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+        
+s = "let object = {link0:\""+videos[0]+"\", link1:\""+videos[1]+"\", link2:\""+videos[2]+"\", link3:\""+videos[3]+"\", link4:\""+videos[4]+"\", link5:\""+videos[5]+"\", link6:\""+videos[6]+"\", link7:\""+videos[7]+"\", link8:\""+videos[8]+"\", link9:\""+videos[9]+"\"}"
+        
+delete_first_line("script.js")
+line_prepender("script.js", s)
+
+
+# In[ ]:
+
+
+for i in range(10):
+    subprocess.run('mkdir '+ str(i) + '; cd ' + str(i) + '; echo "import subprocess\nsubprocess.run(\'youtube-dl -f worstvideo[ext=mp4] '+videos[i]+'; scenedetect --input *.mp4 detect-content save-images; magick *.jpg images.gif; rm -rf *.jpg\', shell=True)" > ' + str(i) + '.py; '
+                , shell=True)
+
+# subprocess.run('mkdir ' + str(i) + '; cd ' + str(i) + '; youtube-dl -f worstvideo[ext=mp4] '
+#                    + videos[i] + '; scenedetect --input *.mp4 detect-content save-images; '
+#                    + 'magick *.jpg images.gif; '+ 'rm -rf *.jpg', shell=True)
+#     subprocess.run('scenedetect --input *.mp4 detect-content save-images', shell=True)
+#     subprocess.run('magick *.jpg images.gif', shell=True)
+#     subprocess.run('rm -rf *.jpg', shell=True)
+
+subprocess.run('cd 0; python3 0.py & cd .. ; cd 1; python3 1.py & cd .. ; cd 2; python3 2.py & cd .. ; cd 3; python3 3.py & cd .. ; cd 4; python3 4.py & cd .. ; cd 5; python3 5.py & cd .. ; cd 6; python3 6.py & cd .. ; cd 7; python3 7.py & cd .. ; cd 8; python3 8.py & cd .. ; cd 9; python3 9.py', shell=True)
+
+
+# In[ ]:
+
+
+
+
---
+++ Final_Project/htmlUI_IG/script.js
@@ -0,0 +1,35 @@
+let object = {link0:"https://www.youtube.com/watch?v=9jLN8MQRQh0", link1:"https://www.youtube.com/watch?v=yj2oWUUpzaU", link2:"https://www.youtube.com/watch?v=K3aFXf74UO4", link3:"https://www.youtube.com/watch?v=HQl-agd384E", link4:"https://www.youtube.com/watch?v=2HPZ3hyVL7Q", link5:"https://www.youtube.com/watch?v=Mba_Uezk_3o", link6:"https://www.youtube.com/watch?v=f-RqOTTSThI", link7:"https://www.youtube.com/watch?v=254BlITxpUs", link8:"https://www.youtube.com/watch?v=GoHM5V9C-H0", link9:"https://www.youtube.com/watch?v=iyFe0M0zQ0g"}
+
+
+function function0() {
+  window.location.assign(object.link0);
+}
+function function1() {
+  window.location.assign(object.link1);
+}
+function function2() {
+  window.location.assign(object.link2);
+}
+function function3() {
+  window.location.assign(object.link3);
+}
+function function4() {
+  window.location.assign(object.link4);
+}
+function function5() {
+  window.location.assign(object.link5);
+}
+function function6() {
+  window.location.assign(object.link6);
+}
+
+function function7() {
+  window.location.assign(object.link7);
+}
+
+function function8() {
+  window.location.assign(object.link8);
+}
+function function9() {
+  window.location.assign(object.link9);
+}
