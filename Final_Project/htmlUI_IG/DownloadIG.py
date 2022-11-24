f742c6e9c58332f238d17810f702311166a601e6
GxhCHULLL <yiyanglin0102@gmail.com>
2022-11-24 12:00:56 -0500

    Created IG Add to video link function

--- Final_Project/htmlUI_IG/DownloadIG.py
+++ Final_Project/htmlUI_IG/DownloadIG.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 #!/usr/bin/env python
@@ -11,6 +11,9 @@ import os, sys, re, subprocess, requests, numpy as np, glob
 from instaloader import Instaloader, Profile, instaloader
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+
+
+subprocess.run(('rm -rf 0 1 2 3 4 5 6 7 8 9'), shell=True)
 
 inputString = 'airplane'
 keyword = '#' + inputString
@@ -34,6 +37,33 @@ for i, name in enumerate(fileNameList):
     subprocess.run('mv ' + inputString +'\/' + name + ' '+ str(i), shell=True)
 
 subprocess.run('rmdir ' + inputString, shell=True)
+
+
+# In[ ]:
+
+
+
+
+
+# In[22]:
+
+
+def delete_first_line(filename):
+    with open(filename, 'r') as fin:
+        data = fin.read().splitlines(True)
+    with open(filename, 'w') as fout:
+        fout.writelines(data[1:])
+        
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+        
+s = "let object = {link0:\""+os.getcwd() + '/0/' +fileNameList[0]+"\", link1:\""+os.getcwd() + '/1/' +fileNameList[1]+"\", link2:\""+os.getcwd() + '/2/' +fileNameList[2]+"\",  link3:\""+os.getcwd() + '/3/' +fileNameList[3]+"\", link4:\""+os.getcwd() + '/4/' +fileNameList[4]+"\", link5:\""+os.getcwd() + '/5/' +fileNameList[5]+"\", link6:\""+os.getcwd() + '/6/' +fileNameList[6]+"\", link7:\""+os.getcwd() + '/7/' +fileNameList[7]+"\", link8:\""+os.getcwd() + '/8/' +fileNameList[8]+"\", link9:\""+os.getcwd() + '/9/' +fileNameList[9]+"\"}"
+        
+delete_first_line("script.js")
+line_prepender("script.js", s)
 
 
 # In[5]:
